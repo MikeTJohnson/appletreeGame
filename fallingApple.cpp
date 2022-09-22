41827e4a1e19f06670f94bc9f72ca1aae79e1314
MikeTJohnson <mtjzdd@gmail.com>
2022-09-22 11:49:19 -0600

    Reorganized files, added gitignore, removed xcode and build folders from git

--- fallingApple.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  fallingApple.cpp
-//  testSFML
-//
-//  Created by Michael Johnson on 9/21/22.
-//
-
-#include "fallingApple.hpp"
-
--- thrownApple1.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  thrownApple.cpp
-//  testSFML
-//
-//  Created by Michael Johnson on 9/21/22.
-//
-
-#include "thrownApple.hpp"
